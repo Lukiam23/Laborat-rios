d60d4de2d23de09bbafbc205909a185f4b3950a0
Lukiam23 <mscorreia95@gmail.com>
2018-03-21 15:10:38 -0300

    Término da quinta questão

--- Laboratório2/5/Teste.java
+++ Laboratório2/5/Teste.java
@@ -1,17 +1,33 @@
 public class Teste{
 	public static void main(String [] args){
-		Conjunto A = new Conjunto(50);
-		Conjunto B = new Conjunto(40);
+		Conjunto conjuntoA = new Conjunto(50);
+		Conjunto conjuntoB = new Conjunto(40);
 		
 
-		for(int i = 0;i<50;i++){
-			A.inserirElemento(i);
+		for(int i = 0;i<25;i++){
+			conjuntoA.inserirElemento(i);
 		} 	
 
 		for(int j = 0;j<40;j++){
-			A.inserirElemento(i);
+			conjuntoB.inserirElemento(j);
 		} 	
-		Conjunto AuB = A.conjuntoUniao(B);
-		AuB.barramento();
+		
+		Conjunto conjuntoAuB = conjuntoA.conjuntoUniao(conjuntoB);
+		Conjunto conjuntoAUBNegado = conjuntoAuB.barramento();
+		Conjunto notA = conjuntoA.barramento();
+		Conjunto notB = conjuntoB.barramento();
+		Conjunto notAIntersecaoNotB = notA.conjuntoIntersecao(notB);
+		System.out.println("Saida do conjunto ~(AUB): ");
+		conjuntoAUBNegado.showElementos();
+		System.out.println("Saida do conjunto ~A /\\ ~B: ");
+		notAIntersecaoNotB.showElementos();
+
+		Conjunto conjuntoAInterB = conjuntoA.conjuntoIntersecao(conjuntoB);
+		Conjunto conjuntoAInterBNegado = conjuntoAInterB.barramento();
+		Conjunto notAUNotB = notA.conjuntoUniao(notB);
+		System.out.println("Saida do conjunto ~(A/\\B): ");
+		conjuntoAInterBNegado.showElementos();
+		System.out.println("Saida do conjunto ~AU~B: ");
+		notAUNotB.showElementos();
 	}
 }
